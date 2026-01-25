e601259a61235d15636a8c9e7af6f53724a22c9b
freekof <freekof8@gmail.com>
2026-01-25 13:10:13 +0800

    Suppress context menu on overlay

--- ScreenCaptureTool/OverlayForm.cs
+++ ScreenCaptureTool/OverlayForm.cs
@@ -489,6 +489,16 @@ namespace ScreenCaptureTool
             }
         }
 
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_CONTEXTMENU = 0x007B;
+            if (m.Msg == WM_CONTEXTMENU)
+            {
+                return;
+            }
+            base.WndProc(ref m);
+        }
+
         protected override void OnClosed(EventArgs e)
         {
             lock (OpenSync)
